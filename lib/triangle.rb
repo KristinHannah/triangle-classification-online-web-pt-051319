565100e3f189a9dd5ec7642af0498bbd872d5bbd
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-16 14:29:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -35,7 +35,8 @@ class Triangle
        end 
      else 
        raise TriangleError
-       resuce TriangleError
+       resuce TriangleError => error 
+        puts error.message
      end 
   end 
  
